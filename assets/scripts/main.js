6ab1c71c2077466d369c68289d915c7b7a7b7e9b
YoanMorel <yoan.morel@live.fr>
2019-04-10 16:36:05 +0200

    Partie questions/réponses terminée

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,43 +1,106 @@
 //Quizz
-var questionsList =
-  ['question,rep1,rep2,rep3,r,rep4',
-  'question,rep1,rep2,r,rep3,rep4',
-  'question,rep1,r,rep2,rep3,rep4',
-  'question,rep1,rep2,rep3,r,rep4',
-  'question,rep1,r,rep2,rep3,rep4',
+var questionsList = ['A quoi pense Paul le matin en arrivant à la Manu ?,Codding or not codding,A des modals dans des modals,A la bonne tasse de café fort qu\'aura fait Quentin,r,Comment je fais fonctionner ce p***** de Wine ?!',
+  'Quelle polémique toune actuellement autour de Laura ?,Elle aime autant Marvel que le DC Universe,Elle n\'aime pas Dragon Ball Z,r,Elle donne trop de coups de coude à ses voisin de table,Il n\'y en a pas. Quelle idée ! Elle est parfaite !',
+  'Qu\'est ce que Damien utilise obstinément en JS et tout cela en vain ?,===,r,**,>>,<<',
+  'A qui appartient le PC que Manu a piraté ? (CYBER SECURITE !),Anousone,Laura,Quentin,r,Alexandre Denurra',
+  'Question pour Patrick : de quelle équipe de football notre bien aimé Jean-Pierre Foucault national est-il fan ?,Olympique de Marseille,r,Paris Saint Germain,Olympique Lyonnais,Le Havre Athlétique Club',
   'question,rep1,rep2,r,rep3,rep4',
   'question,rep1,rep2,rep3,rep4,r',
   'question,rep1,r,rep2,rep3,rep4',
   'question,rep1,rep2,rep3,r,rep4',
-  'question,rep1,r,rep2,rep3,rep4']; // Tableau de chaines contenant questions et réponses
-var checkDone = [0,0,0,0,0,0,0,0,0,0,0]; // Pour vérifier si checkAnswer a déjé été appelé
-var myQuizz = document.getElementsByTagName('li'); // Pour remplir la liste de réponses
-var prevBtn = document.getElementById('prev'); // Pour mettre en surbrillance après réponse
-var nextBtn = document.getElementById('next'); // Pour mettre en surbrillance après réponse
-var index = 0; // Pour parcourir les tableaux
+  'question,rep1,r,rep2,rep3,rep4'
+]; // Tableau de chaines contenant questions et réponses
+
+var userIdAnswer = []; // Tableau contenant l'ID des LI cliquées
+var checkDoneTab = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // Pour vérifier si checkAnswer a déjé été appelée
+var prevBtn = document.getElementById('btnPrev'); // Pour mettre en surbrillance après réponse
+var nextBtn = document.getElementById('btnNext'); // Pour mettre en surbrillance après réponse
+var liPointer = document.getElementsByTagName('li'); // Pointeur vers tous les elem LI
+var indexQuestion = 0; // Pour parcourir les tableaux - Question en cours
 var rightAnswer = ''; // Chaine qui contiendra la bonne réponse
 
-function makeQuizz() { // Fabrique à question-réponses
-  question = questionsList[index].split(','); //découpe la chaine
-  rightAnswer = question[question.indexof('r') - 1]; //récupère l'indice de la bonne réponse
-  //crée les listes de réponses
+prevBtn.style = 'display: none'; // Set le display du BTN Prev dès le chargement de la page
+
+function makeQuizz() { // Fabrique à questions-réponses
+  var answers = questionsList[indexQuestion].split(','); // Découpe la chaine
+  rightAnswer = answers[answers.indexOf('r') - 1]; // Récupère l'indice de la bonne réponse
+  answers.splice(answers.indexOf('r'), 1); // Elimine le Flag
+  var question = answers.splice(0, 1);
+  for (let j = 0; j < answers.length; j++) { // Remplissage du questionnaire
+    document.getElementsByTagName('li')[j].textContent = answers[j];
+  }
+  document.getElementById('question').textContent = question; // affichage de la question
 }
 
 function checkAnswer(elem) { // Va vérifier si t'as la culture en toi
-  checkDone.splice(index, 1, 1); // T'as cliqué, c'est trop tard
-  if (elem.textContent == rightAnswer) {
-    elem.className = ' list-group-item-success';
-    //rendre les autres elem incliquable
+  checkDoneTab.splice(indexQuestion, 1, 1); // T'as cliqué, c'est trop tard
+  userIdAnswer.splice(indexQuestion, 1, elem.id);
+  if (elem.textContent == rightAnswer) { // Colorise en fonction de la réponse
+    elem.classList.add('list-group-item-success');
   } else {
-    elem.className = ' list-group-item-danger';
+    elem.classList.add('list-group-item-danger');
     //rendre les autres elem incliquable
+  }
+  supprOnclick();
+}
+
+function supprOnclick() {
+  for (let i = 0; i <= 3; i++) {
+    liPointer[i].removeAttribute('onclick');
+  }
+}
+
+function setOnclick() {
+  for (let i = 0; i <= 3 ; i++) {
+    liPointer[i].setAttribute('onclick', 'checkAnswer(this)');
+  }
+}
+
+function resetColor() { // Fonction qui boucle sur les class pour reset les couleurs
+  for (let i = 0; i <= 3; i++) {
+    liPointer('li')[i].classList.remove('list-group-item-success');
+  }
+  for (let j = 0; j <= 3; j++) {
+    liPointer('li')[j].classList.remove('list-group-item-danger');
   }
 }
 
 function nextQuestion(elem) {
-
+  resetColor();
+  indexQuestion++; // Incrémentation de l'index des questions
+  prevBtn.style = '';
+  if (indexQuestion <= checkDoneTab.length) { // Check si il reste des questions à afficher
+    makeQuizz();
+    if (checkDoneTab[indexQuestion] == 1) {
+      supprOnclick();
+      if (document.getElementById(userIdAnswer[indexQuestion]).textContent == rightAnswer) {
+        document.getElementById(userIdAnswer[indexQuestion]).classList.add('list-group-item-success');
+      } else {
+        document.getElementById(userIdAnswer[indexQuestion]).classList.add('list-group-item-danger');
+      }
+    } else {
+      setOnclick();
+    }
+  }
 }
 
-function prevQuestion(elem) {
-
+function prevQuestion() {
+  resetColor();
+  if (indexQuestion != 0) {
+    indexQuestion--; // Décrémentation de l'index des questions
+    makeQuizz();
+    if (indexQuestion == 0) {
+      prevBtn.style = 'display: none;';
+    }
+    if (checkDoneTab[indexQuestion] == 1) {
+      supprOnclick();
+      if (document.getElementById(userIdAnswer[indexQuestion]).textContent == rightAnswer) {
+        document.getElementById(userIdAnswer[indexQuestion]).classList.add('list-group-item-success');
+      } else {
+        document.getElementById(userIdAnswer[indexQuestion]).classList.add('list-group-item-danger');
+      }
+    } else {
+      setOnclick();
+    }
+  }
 }
